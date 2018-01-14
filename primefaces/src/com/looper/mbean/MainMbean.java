77635665c1079e3e19b79b34efc80a246ce6f36f
menkung <wadcharapong.men@gmail.com>
2018-01-15 03:24:36 +0700

    Get start project JSF primefaces

---
+++ primefaces/src/com/looper/mbean/MainMbean.java
@@ -0,0 +1,20 @@
+package com.looper.mbean;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainMbean {
+	
+
+	@PostConstruct
+	public void init() {
+		//init when request to pages
+		
+		
+	}
+	
+	
+}
